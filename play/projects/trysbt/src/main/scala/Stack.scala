d239bbbca52c0b50102e3f7f1c54da1af93c2705
c10t <c10t@fa.ke>
2019-01-21 06:19:45 +0900

    immutable stack

---
+++ play/projects/trysbt/src/main/scala/Stack.scala
@@ -0,0 +1,26 @@
+// +A covariant with type A
+trait Stack[+A] {
+  // "E >: A" means A extends E
+  def push[E >: A](e: E): Stack[E]
+  def top: A
+  def pop: Stack[A]
+  def isEmpty: Boolean
+}
+
+class NonEmptyStack[+A](private val first: A, private val rest: Stack[A]) extends Stack[A] {
+  def push[E >: A](e: E): Stack[E] = new NonEmptyStack[E](e, this)
+  def top: A = first
+  def pop: Stack[A] = rest
+  def isEmpty: Boolean = false
+}
+
+case object EmptyStack extends Stack[Nothing] {
+  def push[E >: Nothing](e: E): Stack[E] = new NonEmptyStack[E](e, this)
+  def top: Nothing = throw new IllegalArgumentException("empty stack")
+  def pop: Nothing = throw new IllegalArgumentException("empty stack")
+  def isEmpty: Boolean = true
+}
+
+object stack {
+  def apply(): Stack[Nothing] = EmptyStack
+}
