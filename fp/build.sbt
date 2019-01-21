01eae92cb63f8d5e04cee85c962df993a445aa5a
c10t <c10t@fa.ke>
2019-01-22 08:28:58 +0900

    hof

---
+++ fp/build.sbt
@@ -0,0 +1,4 @@
+name := "fpin"
+
+scalaVersion := "2.12.8"
+version := "0.0.1"
--- fp/src/main/scala/GettingStarted.scala
+++ fp/src/main/scala/GettingStarted.scala
@@ -3,10 +3,30 @@ object MyModule {
     if (n < 0) -n
     else n
 
-  private def formatAbs(x: Int) = {
-      val msg = "The absolute value of %d is %d"
-      msg.format(x, abs(x))
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int = if (n <= 0) acc else go(n - 1, n * acc)
+    go(n, 1)
   }
 
-  def main(args: Array[String]): Unit = println(formatAbs(-42))
+  // exercise 2.1
+  def fibb(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc1: Int, acc2: Int): Int = {
+      if (n <= 1) acc1 else go(n - 1, acc2, acc1 + acc2)
+    }
+
+    go(n, 0, 1)
+  }
+
+  private def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+    println(formatResult("fibonacci number", 6, fibb))
+  }
 }
