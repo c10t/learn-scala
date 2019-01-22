c8b5c94eeb84043bf92b35c8d2264e6699ffabcc
c10t <c10t@fa.ke>
2019-01-23 03:44:17 +0900

    chapter 2

--- fp/src/main/scala/GettingStarted.scala
+++ fp/src/main/scala/GettingStarted.scala
@@ -19,6 +19,39 @@ object MyModule {
     go(n, 0, 1)
   }
 
+  def findFirst[A](as: Array[A], p => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  // exercise 2.2
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= as.length) true
+      else if (!ordered(as(n + 1), as(n))) false
+      else loop(n + 1)
+    }
+
+    loop(0)
+  }
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = (b: B) => f(a, b)
+
+  // exercise 2.3
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A) => (b: B) => f(a, b)
+
+  // exercise 2.4
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a: A, b :B) => f(a)(b)
+  
+  // exercise 2.5
+  def compose[A, B, C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
+
   private def formatResult(name: String, n: Int, f: Int => Int) = {
     val msg = "The %s of %d is %d."
     msg.format(name, n, f(n))
